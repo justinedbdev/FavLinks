00060874239e214d127a06633bdbad597a75b6b0
Justine Debard <justine_debard@yahoo.com>
2023-06-05 18:21:40 +0200

    create link between API & entities

--- src/Entity/Link.php
+++ src/Entity/Link.php
@@ -5,8 +5,10 @@ namespace App\Entity;
 use App\Repository\LinkRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: LinkRepository::class)]
+#[ApiResource]
 class Link
 {
     #[ORM\Id]
