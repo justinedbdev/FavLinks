d2e82ea97a06e216be38eaaab38a8bd8f21b7323
Justine Debard <justine_debard@yahoo.com>
2023-06-06 11:21:47 +0200

    Add Fixture Bundle & Faker + Creation HomeController

---
+++ public/scripts/links.js
@@ -0,0 +1,13 @@
+const urlApi = "/api";
+
+function getLinks() {
+  const headers = new Headers();
+
+  const init = {
+    method: "GET",
+    headers: headers,
+  };
+
+  const url = urlApi + "/links";
+  fetch("");
+}
